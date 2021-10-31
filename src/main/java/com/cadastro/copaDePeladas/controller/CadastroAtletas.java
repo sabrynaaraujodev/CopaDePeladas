2abac7df8b0bd7a5c8df9441b07ae10c35d2e3dc
Sabryna Araujo <sabrynaaraujodev@gmail.com>
2021-10-30 23:43:18 -0300

    implementando view para construcao de tela

--- src/main/java/com/cadastro/copaDePeladas/controller/CadastroAtletas.java
+++ src/main/java/com/cadastro/copaDePeladas/controller/CadastroAtletas.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.jws.WebParam;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +24,13 @@ public class CadastroAtletas  {
     @Autowired
     private CadastroService cadastroService;
 
+    @RequestMapping(value = "/tela" ,  method = RequestMethod.GET)
+    public ModelAndView getAtletas(){
+        ModelAndView teste = new ModelAndView("tela");
+        List<Atletas> atletas = cadastroService.listar();
+        teste.addObject("tela",atletas);
+        return teste;
+    }
 
     @PostMapping(path = "/novo")
     public Atletas adicionaAtleta(@Valid @RequestBody Atletas atletas) {
